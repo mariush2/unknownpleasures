14fcaa6884c18c50571091f22533023ac147b6f9
Marius Tobiassen <MariusTobi_98@hotmail.com>
2019-03-26 09:51:04 +0100

    Added triangle waves, still not looking as it should

--- sketch.js
+++ sketch.js
@@ -20,20 +20,20 @@ function draw() {
 function Line(y) {
 	stroke(255);
 	// Create points along the line we are on now, with noise
-				let prev = y;
+	let prev = y;
+	let p = random(3, 4.5);
+	let a = 20;
 	for (let x = 0; x < windowWidth - offsetWidth; x++) {
 		let n;		
 		if (x >= (windowWidth - offsetWidth) / 3 && x <= (windowWidth - offsetWidth) - (windowWidth - offsetWidth) / 3) {
-			if (x >= (windowWidth - offsetWidth) / 2) {
-				n = random(-0.5, sin((x / 16) + random(1, 10))*3) + 0.22;
-			} else {
- 				n = random(-sin((x / 16) + random(1, 10))*3, 0.5) - 0.22;	
-			}
-										
+			let newx = map(x, (windowWidth - offsetWidth) / 3, (windowWidth - offsetWidth) - (windowWidth - offsetWidth) / 3, 0, PI * 2);
+			n =  ((2 * a) / PI) * asin(sin(((2 * PI) / p) * newx));
+			n += random(-0.5, 0.5);
+			point(x, prev + n);
 		} else {
 			n = random(-0.5, 0.5);
+			point(x, prev + n);
+			prev = prev + n
 		}
-		point(x, prev + n);
-		prev = prev + n
 	}
 }
